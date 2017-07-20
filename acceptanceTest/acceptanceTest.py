3cf33be87645ef82da9f50a904b5f4d216449df3
Jan Vermeir <jan@192.168.2.107>
2017-07-20 17:13:15 +0200

    first version

---
+++ acceptanceTest/acceptanceTest.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+
+TARGET_DIR = "target"
+
+
+class OutputContains4JpgFilesIn2Folders(unittest.TestCase):
+    def testOutputContains4JpgFilesIn2Folders(self):
+        expectedOutputFiles = [TARGET_DIR + '/201701/20170101_123456.jpg',
+                               TARGET_DIR + '/201701/IMG-20170101-WA0000.jpg',
+                               TARGET_DIR + '/201702/20170201_123456.jpg',
+                               TARGET_DIR + '/201702/IMG-20170201-WA0000.jpg']
+        actualOutputFiles = set(self.getFilesInTargetFolder(TARGET_DIR))
+        self.assertItemsEqual(expectedOutputFiles, actualOutputFiles, 'expected \n' + ', '.join(expectedOutputFiles) + '\n does not match actual \n' + ', '.join(actualOutputFiles))
+
+
+    def getFilesInTargetFolder(self, rootfolder):
+        # Copied from https://stackoverflow.com/a/19308592/7290832
+        file_paths = []
+        for root, directories, files in os.walk(rootfolder):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)
+        return file_paths
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ findAndOrganizeNewPhotos.py
@@ -0,0 +1,46 @@
+import os
+import fnmatch
+import shutil
+import re
+
+
+class FindAndOrganizeNewPhotos():
+    def getJpgFiles(self, rootDir):
+        newFiles = dict()
+        for root, dirnames, filenames in os.walk(rootDir):
+            for filename in fnmatch.filter(filenames, '*.[jJ][pP][gG]'):
+                newFiles[filename] = os.path.join(root, filename)
+        return newFiles
+
+    def findDate(self, fileName):
+        # 20160319_195252.jpg
+        # IMG-20170525-WA0000.jpg
+        pattern = r'.*(201\d\d\d\d\d)[_-].*'
+        date = re.findall(pattern, fileName)
+        if len(date) > 0 and len(date[0]) == 8:
+            return date[0][:6]
+        else:
+            return ""
+
+    def copyPhotos(self):
+        fromPhoneDir = "acceptanceTest/testData/input"
+        fromPhone = self.getJpgFiles(fromPhoneDir)
+
+        allPhotosDir = "acceptanceTest/testData/currentPhotos"
+        allPhotos = self.getJpgFiles(allPhotosDir)
+
+        newPhotos = set(fromPhone.keys()) - set(allPhotos.keys())
+
+        photosByDateDir = "target"
+        for foto in newPhotos:
+            source = fromPhone[foto]
+            targetDir = photosByDateDir + "/" + self.findDate(source)
+            if not os.path.exists(targetDir):
+                os.makedirs(targetDir)
+            shutil.copy(fromPhone[foto], targetDir)
+
+
+shutil.rmtree("target")
+os.mkdir("target")
+finder = FindAndOrganizeNewPhotos()
+finder.copyPhotos()
