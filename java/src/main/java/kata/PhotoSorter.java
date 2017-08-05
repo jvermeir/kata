15e19fb47fd90a8d9ec19ba5fe322422084016db
Jan Vermeir <jvermeir@hotmail.com>
2017-08-05 21:45:05 +0200

    Java version

---
+++ java/src/main/java/kata/PhotoSorter.java
@@ -0,0 +1,122 @@
+package kata;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.logging.Logger;
+
+public class PhotoSorter {
+    private final String sourceDirectory;
+    private final String outputDirectory;
+    private final String currentPhotosDirectory;
+    private static final Logger LOGGER = Logger.getLogger(PhotoSorter.class.getName());
+
+    public PhotoSorter(String sourceDirectory, String outputDirectory) throws IOException {
+        this.sourceDirectory = sourceDirectory;
+        this.outputDirectory = outputDirectory;
+        this.currentPhotosDirectory = null;
+        createOutputDirectory(outputDirectory);
+    }
+
+    public PhotoSorter(String sourceDirectory, String outputDirectory, String currentPhotosDirectory) throws IOException {
+        this.sourceDirectory = sourceDirectory;
+        this.outputDirectory = outputDirectory;
+        this.currentPhotosDirectory = currentPhotosDirectory;
+        createOutputDirectory(outputDirectory);
+    }
+
+    private void createOutputDirectory(String outputDirectory) throws IOException {
+        FileUtils.deleteDirectory(new File(outputDirectory));
+        new File(outputDirectory).mkdirs();
+    }
+
+    protected String extractYearAndMonthFromFileName(String fileNameWithDateAndTimeStamp) {
+        String[] fileNameParts = fileNameWithDateAndTimeStamp.split("/");
+        String fileName = fileNameParts[fileNameParts.length - 1];
+        if (fileName.startsWith("IMG-")) {
+            fileName = fileName.substring(4);
+        }
+        String[] nameParts = fileName.split("_");
+        String date = nameParts[0];
+        String dateAsString = date.substring(0, Math.min(6, date.length()));
+        if (isValidDate(dateAsString)) {
+            return dateAsString;
+        } else {
+            return "";
+        }
+    }
+
+    private boolean isValidDate(String dateAsString) {
+        try {
+            new SimpleDateFormat("yyyyMM").parse(dateAsString);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    protected Collection<File> findFiles() {
+        return FileUtils.listFiles(new File(sourceDirectory), new NewFileFilter(currentPhotosDirectory), TrueFileFilter.INSTANCE);
+    }
+
+    protected String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void copyFilesToDirectories() {
+        Collection<File> files = findFiles();
+        for (File file : files) {
+            String yearAndMonth = extractYearAndMonthFromFileName(file.getName());
+            String fileName = file.getName();
+            String outputDirForCurrentFile = outputDirectory + "/" + yearAndMonth + "/";
+            new File(outputDirForCurrentFile).mkdirs();
+            try {
+                FileUtils.copyFile(file, new File(outputDirForCurrentFile + fileName));
+            } catch (IOException e) {
+                LOGGER.info("Error copying file " + file);
+            }
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        if (args.length < 2) {
+            System.err.println("Usage java kata.PhotoSorter <directoryWithUnsortedPhotos> <directoryForSortedPhotos> [directoryForPhotosThatWereCopiedEarlier]");
+            System.exit(-1);
+        }
+        String directoryWithUnsortedPhotos = args[0];
+        String directoryForSortedPhotos = args[1];
+        PhotoSorter photoSorter = null;
+        if (args.length == 2) {
+            photoSorter = new PhotoSorter(directoryWithUnsortedPhotos, directoryForSortedPhotos);
+        } else {
+            String directoryForPhotosThatWereCopiedEarlier = args[2];
+            photoSorter = new PhotoSorter(directoryWithUnsortedPhotos, directoryForSortedPhotos, directoryForPhotosThatWereCopiedEarlier);
+        }
+        photoSorter.copyFilesToDirectories();
+    }
+}
+
+class NewFileFilter implements IOFileFilter {
+
+    private final String currentFilesDirectory;
+    public NewFileFilter(String currentFilesDirectory) {
+        this.currentFilesDirectory = currentFilesDirectory;
+    }
+
+    @Override
+    public boolean accept(File file) {
+        String fullName = currentFilesDirectory + "/" + file.getName();
+        return !new File(fullName).exists();
+    }
+
+    @Override
+    public boolean accept(File file, String s) {
+        return false;
+    }
+}
---
+++ java/src/test/java/kata/FindAndOrganizeNewPhotosTest.java
@@ -0,0 +1,87 @@
+package kata;
+
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FindAndOrganizeNewPhotosTest {
+
+    public static final String INPUT_DIRECTORY = "../validate/testData/input/";
+    public static final String TARGET_DIRECTORY = "../validate/target/";
+    public static final String CURRENT_PHOTOS_DIRECTORY = "../validate/testData/currentPhotos/";
+
+    @Test
+    public void testYearAndMonthAreDerivedFromFileName() {
+        try {
+            PhotoSorter photoSorter = new PhotoSorter("", "");
+            Assert.assertEquals("201701", photoSorter.extractYearAndMonthFromFileName("resources/testData/input/20170101_123456.jpg"));
+            Assert.assertEquals("201702", photoSorter.extractYearAndMonthFromFileName("20170201_123456.jpg"));
+            Assert.assertEquals("201703", photoSorter.extractYearAndMonthFromFileName("resources/testData/input/IMG-20170301-WA0000.jpg"));
+        } catch (IOException e) {
+            Assert.fail("Unexpected exception " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testYearAndMonthAreEmptyIfFileNameDoesntContainADate() {
+        try {
+            PhotoSorter photoSorter = new PhotoSorter("", "");
+            Assert.assertEquals("", photoSorter.extractYearAndMonthFromFileName(""));
+            Assert.assertEquals("", photoSorter.extractYearAndMonthFromFileName("aap"));
+        } catch (IOException e) {
+            Assert.fail("Unexpected exception " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testFiveFilesAreReadFromTestInput() {
+        try {
+            Assert.assertEquals(5, new PhotoSorter(INPUT_DIRECTORY, "").findFiles().size());
+        } catch (IOException e) {
+            Assert.fail("Unexpected exception " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testOutputDirectoryIsEmptyWhenPhotoSorterIsCreated() {
+        try {
+            PhotoSorter photoSorter = new PhotoSorter("source", TARGET_DIRECTORY);
+            File outputDirectory = new File(photoSorter.getOutputDirectory());
+            Assert.assertTrue(outputDirectory.exists());
+            Assert.assertEquals(0, FileUtils.listFiles(outputDirectory, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE).size());
+        } catch (IOException e) {
+            Assert.fail("Unexpected exception " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testOutputContainsCorrectFiles() {
+        try {
+            PhotoSorter photoSorter = new PhotoSorter(INPUT_DIRECTORY, TARGET_DIRECTORY);
+            photoSorter.copyFilesToDirectories();
+            Assert.assertTrue(new File("../validate/target/" + "201701/20170101_123456.jpg").exists());
+            Assert.assertTrue(new File("../validate/target/" + "201701/20170102_123456.jpg").exists());
+            Assert.assertTrue(new File("../validate/target/" + "201701/IMG-20170101-WA0000.jpg").exists());
+            Assert.assertTrue(new File("../validate/target/" + "201702/20170201_123456.jpg").exists());
+            Assert.assertTrue(new File("../validate/target/" + "201702/IMG-20170201-WA0000.jpg").exists());
+        } catch (IOException e) {
+            Assert.fail("Unexpected exception " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPhotosInCurrentFolderAreNotCopied() {
+        try {
+            PhotoSorter photoSorter = new PhotoSorter(INPUT_DIRECTORY, TARGET_DIRECTORY, CURRENT_PHOTOS_DIRECTORY);
+            photoSorter.copyFilesToDirectories();
+            Assert.assertEquals(2, new File(TARGET_DIRECTORY + "/201701").listFiles().length);
+        } catch (IOException e) {
+            Assert.fail("Unexpected exception " + e.getMessage());
+        }
+    }
+}
